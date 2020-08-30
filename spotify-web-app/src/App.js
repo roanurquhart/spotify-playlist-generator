fd0459bd41539d62a00647933df63578df3ed6e3
roanurquhart <roanurquhart@gmail.com>
2020-08-30 19:52:26 -0400

    Got initial auth token and initial API call working

--- spotify-web-app/src/App.js
+++ spotify-web-app/src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './index.css';
+import SpotifyWebApi from './spotify-web-api.js';
+
+const spotifyApi = new SpotifyWebApi()
 
 class App extends React.Component {
-    displaySomeText() {
-        return 'Hello there friends'
+    constructor (props) {
+        super(props);
+        this.state = {
+          name: '',
+        };
     }
+    componentDidMount() {
+        let accessToken =  new URLSearchParams(window.location.search).get('access_token')
+        let artistID = '1z4g3DjTBBZKhvAroFlhOM'
+        fetch('https://api.spotify.com/v1/artists/' + artistID, {
+            headers: {'Authorization': 'Bearer ' + accessToken}
+        }).then(response => response.json())
+        .then(data => this.setState({name: data.name}))
+        
+    }
+
     render() {
         return (
-            <div>
-                {this.displaySomeText()}
+            <div className="App">
+                <h1>Hello World</h1>
+                <h2>{this.state.name}</h2>
             </div>
         )
     }
